4ec5b2640a6be6e6d7a48662913cec1d53ba33d4
MaysWind <i@mayswind.net>
2015-11-15 17:07:27 +0800

    题目详情和竞赛题目详情页支持图表显示提交结果分布

--- website/SDNUOJ/js-source/parts-foreground/foreground-pages-status.js
+++ website/SDNUOJ/js-source/parts-foreground/foreground-pages-status.js
@@ -35,3 +35,74 @@ SDNUOJ.pages.status = (function () {
         }
     };
 })();
+
+SDNUOJ.pages.problemstatistic = (function () {
+    return {
+        init: function () {
+            SDNUOJ.pages.status.init();
+
+            function isNoSubmit() {
+                if (totalCount < 1) {
+                    return true;
+                }
+                
+                for (var i = 0; i < statisticData.length; i++) {
+                    if (statisticData[i].data > 0) {
+                        return false;
+                    }
+                }
+
+                return true;
+            }
+
+            if (!isNoSubmit()) {
+                $.getScript("/static/js/foundation/jquery.flot-0.8.3.min.js", function () {
+                    $.getScript("/static/js/foundation/jquery.flot.pie-0.8.3.min.js", function () {
+                        $.plot("#statistic-chart", statisticData, {
+                            series: {
+                                pie: {
+                                    innerRadius: 0.6,
+                                    show: true,
+                                    label: {
+                                        show: false
+                                    }
+                                }
+                            },
+                            legend: {
+                                show: false
+                            },
+                            grid: {
+                                hoverable: true
+                            }
+                        });
+
+                        $('<span id="statistic-chart-tipcontainer"></span>')
+                            .addClass("pieLabel").addClass("statistic-chart-tipcontainer")
+                            .appendTo($("#statistic-chart"));
+
+                        $("#statistic-chart").bind("plothover", function (event, pos, item) {
+                            if (item) {
+                                var percent = (totalCount > 0 ? item.datapoint[1][0][1] / totalCount * 100 : 0);
+                                var tip = $("<div></div>").addClass("statistic-chart-tip")
+                                    .css("background-color", item.series.color)
+                                    .html(item.series.label + "<br/>" + percent.toFixed(2) + "%");
+                                var top = 100 - 24 - 5;
+                                var left = 125 - (percent >= 100 ? 28 : percent >= 10 ? 25 : 22) - 6;
+                                $("#statistic-chart-tipcontainer").css({
+                                    position: "absolute",
+                                    top: top * $("#statistic-chart").height() / 200,
+                                    left: left * $("#statistic-chart").width() / 250
+                                }).empty().append(tip).fadeIn(200);
+                            }
+                            else {
+                                $("#statistic-chart-tipcontainer").hide().empty();
+                            }
+                        });
+                    });
+                });
+            } else {
+                $("#statistic-chart").hide();
+            }
+        }
+    };
+})();
