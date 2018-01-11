f918d77a40b0abc9d5c5b94186913ea6b06f930c
Azure <961523404@qq.com>
2018-01-11 10:46:03 +0800

    Modify default configuration

--- judge/src/Definition/Configuration.cs
+++ judge/src/Definition/Configuration.cs
@@ -41,14 +41,14 @@ namespace JudgeClient.Definition
             Fetchers.Add(new FetcherProfile()
             {
                 Type = "JudgeClient.SDNU.SDNUFetcher, JudgeClient.Fetcher",
-                AuthenticationURL = "http://www.acmicpc.sdnu.edu.cn/Judge.ashx?action=login",
+                AuthenticationURL = "http://127.0.0.1/judge/login",
                 Username = "httpjudge",
                 Password = "iloveeclan",
-                TaskFetchURL = "http://www.acmicpc.sdnu.edu.cn/Judge.ashx?action=getpending",
-                FetchInterval = 5000,
+                TaskFetchURL = "http://127.0.0.1/judge/getpending",
+                FetchInterval = 3000,
                 FetchTimeout = 5000,
-                DataFetchURL = "http://www.acmicpc.sdnu.edu.cn/Judge.ashx?action=getproblem",
-                ResultSubmitURL = "http://www.acmicpc.sdnu.edu.cn/Judge.ashx?action=updatestatus",
+                DataFetchURL = "http://127.0.0.1/judge/getproblem",
+                ResultSubmitURL = "http://127.0.0.1/judge/updatestatus",
                 DataAccessorProfile = new DataAccessorProfile()
                 {
                     Type = "JudgeClient.SDNU.SDNUDataAccessor, JudgeClient.Fetcher",
